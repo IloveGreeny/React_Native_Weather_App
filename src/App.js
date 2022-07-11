efe592181136ad4179cd5a7da39ff5b2de4eb000
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-07-11 21:22:48 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
+import { QueryClient,QueryClientProvider,useQuery,useMutation,InvalidateQueries } from "react-query";
 
-function App() {
+
+const queryClient = new QueryClient();
+export default function App() {
+
+
+  async function fetchData() {
+     const res = await fetch("/MockData/data.json");
+     return res.json();
+  }
+
+function Data() {
+    const { data, status } = useQuery("data",fetchData);
+
+    const mutation = useMutation( {
+      onSuccess: () => {
+        queryClient.invalidateQueries("data");
+      },
+    })
+
+    if (status === "loading") {
+      return <p>Loading</p>
+    }
+
+    if(status === "error") {
+      return  <p>Error!</p>
+    }
+
+    return  (
+        <ul>
+          {data.map((datas) => {
+            <li key={data.id}>{data.make}</li>
+          })}
+        </ul>
+    )
+}
+
   return (
-    <div>
-    </div>
+      <QueryClientProvider client={queryClient}>
+        <Data/>
+      </QueryClientProvider>
   );
 }
 
-export default App;
+
