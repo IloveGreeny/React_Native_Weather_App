f05db5344942058579330ffb9c61188fb89f8310
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-07-14 17:45:37 +0400

    Add files via upload

---
+++ src/Lessons/lesson4/project/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from './Components/ Header/Heading';
+
+export default function App() { 
+  return (
+    <div className="App">
+      <Header title="proxy"/>
+    </div>
+  );
+}
+
---
+++ src/Lessons/lesson4/project/Components/ Header/Heading.js
@@ -0,0 +1,150 @@
+
+
+// fetch ,then
+
+import { useState, useEffect } from "react";
+
+export default function Heading() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts?_limit=12`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Posts</h1>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <ul>
+        {data &&
+          data.map(({ id, title }) => (
+            <li key={id}>
+              <h3>{title}</h3>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+
+// async , await
+
+import { useState, useEffect } from "react";
+
+export default function Heading() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/posts?_limit=10`
+        );
+        if (!response.ok) {
+          throw new Error(
+            `HTTP error: The status is ${response.status}`
+          );
+        }
+        let actualData = await response.json();
+        setData(actualData);
+        setError(null);
+      } catch(err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }  
+    }
+    getData()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Posts</h1>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <ul>
+        {data &&
+          data.map(({ id, title }) => (
+            <li key={id}>
+              <h3>{title}</h3>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+
+// axios library
+
+import axios from "axios";
+
+export default function Heading() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+            `https://jsonplaceholder.typicode.com/posts?_limit=10`
+        );
+        setData(response.data);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+  return(
+      <div className="App">
+        <h1>Posts</h1>
+        {loading && <div>A moment please...</div>}
+        {error && (
+            <div>{`There is a problem fetching the post data - ${error}`}</div>
+        )}
+        <ul>
+          {data &&
+              data.map(({ id, title }) => (
+                  <li key={id}>
+                    <h3>{title}</h3>
+                  </li>
+              ))}
+        </ul>
+      </div>
+  )
+}
