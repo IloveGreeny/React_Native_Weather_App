907d84a19f1e96b44a8f4f5c10d20423d3a75478
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-07-14 17:51:34 +0400

    Update App.js

--- src/Lessons/lesson4/project/App.js
+++ src/Lessons/lesson4/project/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Header from './Components/ Header/Heading';
+import Heading from './Components/ Header/Heading';
 
 export default function App() { 
   return (
     <div className="App">
-      <Header/>
+      <Heading/>
     </div>
   );
 }
